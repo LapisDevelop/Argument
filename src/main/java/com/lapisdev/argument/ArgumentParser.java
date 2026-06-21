a0c62faaca0b2b62cbf4f6d5f45c9209fb5b4b84
dylancode <opensource@dylancode.dev>
2026-06-21 12:36:57 +0100

    register parsers in LapisArgument.register

--- src/main/java/com/lapisdev/argument/ArgumentParser.java
+++ src/main/java/com/lapisdev/argument/ArgumentParser.java
@@ -3,5 +3,6 @@ package com.lapisdev.argument;
 import co.aikar.commands.BukkitCommandExecutionContext;
 
 public interface ArgumentParser<T> {
+    Class<T> getType();
     T parse(BukkitCommandExecutionContext ctx);
 }
--- src/main/java/com/lapisdev/argument/LapisArgument.java
+++ src/main/java/com/lapisdev/argument/LapisArgument.java
@@ -4,7 +4,22 @@ import co.aikar.commands.PaperCommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LapisArgument extends JavaPlugin {
-    public static void register(PaperCommandManager commandManager) {
 
+    public static final ArgumentParser<?>[] PARSERS = new ArgumentParser<?>[]{
+
+    };
+
+    public static final ArgumentSuggestion[] SUGGESTIONS = new ArgumentSuggestion[]{
+
+    };
+
+    public static void register(PaperCommandManager commandManager) {
+        for (ArgumentParser<?> parser : PARSERS) {
+            commandManager.getCommandContexts().registerContext((Class)parser.getType(), parser::parse);
+        }
+
+        for (ArgumentSuggestion suggestion : SUGGESTIONS) {
+            commandManager.getCommandCompletions().registerCompletion(suggestion.getName(), suggestion::suggest);
+        }
     }
 }
